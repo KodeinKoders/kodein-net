f1cc2ab6663bc056572d2b4dc555319dc9a77b85
Romain Boisselle <romain@boisselle.dev>
2020-11-02 22:10:23 +0100

    illustrations utils

--- front/src/main/kotlin/net.kodein/pages/home/fragment/Descriptions.kt
+++ front/src/main/kotlin/net.kodein/pages/home/fragment/Descriptions.kt
@@ -186,8 +186,6 @@ private val Description = functionalComponent<DescriptionProps>("Description") {
     }
 }
 
-private val illustrationWidths = listOf(960, 1200, 1440, 1680, 1920, 2400, 2880, 3360, 3840)
-
 interface IllustrationProps : RProps {
     var color: Color
     var image: String
---
+++ front/src/main/kotlin/net.kodein/utils/illustrations.kt
@@ -0,0 +1,3 @@
+package net.kodein.utils
+
+val illustrationWidths = listOf(960, 1200, 1440, 1680, 1920, 2400, 2880, 3360, 3840)
