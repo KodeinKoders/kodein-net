015cf181e6369e6417a58fd4f81efb4faded32fb
Romain Boisselle <romain@boisselle.dev>
2020-11-09 07:27:16 +0100

    Accordion is now a common component

---
+++ front/src/main/kotlin/net.kodein/components/Accordion.kt
@@ -0,0 +1,169 @@
+package net.kodein.components
+
+import kotlinx.css.*
+import kotlinx.css.properties.*
+import net.kodein.charter.kodein
+import net.kodein.utils.flexColumn
+import net.kodein.utils.flexRow
+import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.events.EventListener
+import react.*
+import react.dom.br
+import react.dom.span
+import styled.css
+import styled.styledDiv
+import styled.styledP
+
+
+interface AccordionElementProps : RProps{
+    var title: String
+    var last: Boolean
+    var sub: (RBuilder.() -> Unit)?
+    var borderColor: Color?
+    var fontColor: Color?
+}
+val AccordionElement = functionalComponent<AccordionElementProps>("AccordionElement") { props ->
+    val accordionHeader = useRef<HTMLDivElement?>(null)
+    var isOpen by useState(false)
+
+    val borderColor = props.borderColor ?: Color.kodein.klycine
+    val fontColor = props.fontColor ?: Color.kodein.kaumon
+
+    useEffectWithCleanup {
+        val openTraining = EventListener {
+            isOpen = !isOpen
+        }
+        accordionHeader.current!!.addEventListener("mouseup", openTraining)
+        ({ accordionHeader.current!!.removeEventListener("mouseup", openTraining) })
+    }
+
+    flexColumn {
+        css {
+            if (props.last) borderBottom(1.px, BorderStyle.solid, borderColor.withAlpha(0.5))
+        }
+
+        // Header
+        flexRow {
+            ref = accordionHeader
+            css {
+                width = 100.pct
+                height = 5.rem
+                alignItems = Align.center
+                cursor = Cursor.pointer
+                borderTop(1.px, BorderStyle.solid, borderColor.withAlpha(0.5))
+            }
+
+            styledP {
+                css {
+                    flexGrow = 75.0
+                    flexBasis = FlexBasis.zero
+                    +kodein.intertitre
+                    color = Color.kodein.kaumon
+                }
+
+                +props.title
+            }
+
+            styledDiv {
+                css {
+                    flexGrow = 20.0
+                    flexBasis = FlexBasis.zero
+                    color = fontColor
+                    textAlign = TextAlign.end
+                    minWidth = LinearDimension.fitContent
+                    paddingRight = 1.rem
+
+                    transition(::opacity, duration = .2.s, Timing.linear)
+                    opacity = if (isOpen) 1 else 0
+                }
+
+                props.sub?.invoke(this)
+            }
+
+            child(Chevron) { attrs.isUp = isOpen }
+        }
+
+        // Content
+        styledDiv {
+            css {
+                overflow = Overflow.hidden
+
+                transition(::opacity, duration = .3.s, Timing.linear)
+
+                if (isOpen) {
+                    maxHeight = 50.rem
+                    opacity = 1
+                } else {
+                    maxHeight = 0.rem
+                    opacity = 0
+                }
+            }
+
+            styledP {
+                css {
+                    +kodein.body
+                    color = fontColor
+                    padding(2.rem)
+                }
+
+                props.children()
+                +"Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+                br {  }
+                +"- Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
+                br {  }
+                +"- when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            }
+        }
+    }
+}
+
+private interface ChevronProps : RProps {
+    var isUp: Boolean
+}
+private val Chevron = functionalComponent<ChevronProps>("Chevron") { props ->
+    styledDiv {
+        css {
+            display = Display.flex
+            flexGrow = 5.0
+            flexBasis = FlexBasis.zero
+            minWidth = 2.rem
+            paddingRight = .5.rem
+
+            "span.chevron" {
+                display= Display.inlineBlock
+                position = Position.absolute
+                textAlign = TextAlign.center
+                height = 0.2.rem
+                width = 2.rem
+            }
+            "span.chevron:before" {
+                content = QuotedString("")
+                position = Position.absolute
+                top = 0.px
+                left = 0.px
+                height = 100.pct
+                width = 51.pct
+                backgroundColor = Color.kodein.klycine.withAlpha(0.75)
+                transform { skew(0.deg, (30).deg); }
+            }
+            "span.chevron:after" {
+                content = QuotedString("")
+                position = Position.absolute
+                top = 0.px
+                right = 0.px
+                height = 100.pct
+                width = 50.pct
+                backgroundColor = Color.kodein.klycine.withAlpha(0.75)
+                transform { skew(0.deg, (-30).deg); }
+            }
+            "span.chevron.up:before" {
+                transform { skew(0.deg, (-30).deg); }
+            }
+            "span.chevron.up:after" {
+                transform { skew(0.deg, (30).deg); }
+            }
+        }
+
+        span(if (props.isUp) "chevron up" else "chevron") {}
+    }
+}
--- front/src/main/kotlin/net.kodein/pages/training/fragment/Trainings.kt
+++ front/src/main/kotlin/net.kodein/pages/training/fragment/Trainings.kt
@@ -1,45 +1,20 @@
 package net.kodein.pages.training.fragment
 
 import kotlinx.css.*
-import kotlinx.css.properties.*
+import kotlinx.css.properties.border
 import net.kodein.charter.kodein
+import net.kodein.components.AccordionElement
 import net.kodein.components.contentRow
 import net.kodein.utils.flexColumn
-import net.kodein.utils.flexRow
-import net.kodein.utils.light
-import org.w3c.dom.HTMLDivElement
-import org.w3c.dom.events.EventListener
-import org.w3c.dom.get
-import react.*
-import react.dom.br
-import react.dom.span
+import react.RBuilder
+import react.RProps
+import react.child
+import react.functionalComponent
 import styled.css
-import styled.styledDiv
+import styled.styledImg
 import styled.styledP
 
 val Trainings = functionalComponent<RProps>("Trainings") {
-
-    val traningListContainer = useRef<HTMLDivElement?>(null)
-    var selectedTraining by useState(-1)
-
-    useEffectWithCleanup {
-        val trainingListElement = traningListContainer.current!!
-
-        val events = mutableListOf<() -> Unit>()
-
-        for (trainingId in (0..trainingListElement.childElementCount)) {
-            val trainingItem = trainingListElement.children[trainingId]
-            if (trainingItem != null) {
-                val openTraining = EventListener {
-                    selectedTraining = if (selectedTraining == trainingId) -1 else trainingId
-                }
-                trainingItem.addEventListener("mouseup", openTraining)
-                events.add { trainingItem.removeEventListener("mouseup", openTraining) }
-            }
-        }
-
-        ({ events.forEach { it() } })
-    }
 
     contentRow(
         backgroundColor = Color.kodein.dark, indexPosition = 3, // see [Description] that stops at index 2
@@ -67,177 +42,37 @@ val Trainings = functionalComponent<RProps>("Trainings") {
                 +"We are here to help you to keep your Kotlin knowledge up to date"
             }
 
-            flexColumn {
-                ref = traningListContainer
+            child(AccordionElement) {
+                attrs {
+                    title = "Kotlin for Android"
+                    sub = duration("1 days")                }
+            }
 
-                child(Training) {
-                    attrs {
-                        title = "Kotlin for Android"
-                        duration = 1
-                        isOpen = selectedTraining == 0
-                    }
+            child(AccordionElement) {
+                attrs {
+                    title = "Mastering coroutines"
+                    sub = duration("1 days")
                 }
+            }
 
-                child(Training) {
-                    attrs {
-                        title = "Working with coroutines"
-                        duration = 2
-                        isOpen = selectedTraining == 1
-                    }
-                }
-
-                child(Training) {
-                    attrs {
-                        title = "Kotlin on the backend"
-                        duration = 3
-                        isOpen = selectedTraining == 2
-                        last = true
-                    }
+            child(AccordionElement) {
+                attrs {
+                    title = "Kotlin on the backend"
+                    sub = duration("1 days")
+                    last = true
                 }
             }
         }
     }
 }
 
-
-interface TrainingProps : RProps{
-    var title: String
-    var duration: Int
-    var last: Boolean
-    var isOpen: Boolean
-}
-
-val Training = functionalComponent<TrainingProps>("MenuContent") { props ->
-    flexColumn {
+private fun duration(duration: String): RBuilder.() -> Unit = {
+    styledImg(src = "imgs/stopwatch-kaumon.svg") {
         css {
-            if (props.last) borderBottom(1.px, BorderStyle.solid, Color.kodein.klycine.withAlpha(0.5))
-        }
-
-        flexRow {
-            css {
-                cursor = Cursor.pointer
-                width = 100.pct
-                height = 5.rem
-                borderTop(1.px, BorderStyle.solid, Color.kodein.klycine.withAlpha(0.5))
-            }
-
-            styledP {
-                css {
-                    flexGrow = 80.0
-                    flexBasis = FlexBasis.zero
-                    +kodein.intertitre
-                    color = Color.kodein.kaumon
-                    alignSelf = Align.center
-                }
-
-                +props.title
-            }
-
-            styledP {
-                css {
-                    flexGrow = 10.0
-                    flexBasis = FlexBasis.zero
-                    color = Color.kodein.kaumon
-                    alignSelf = Align.center
-                    justifyContent = JustifyContent.end
-
-                    transition(::opacity, duration = .2.s, Timing.linear)
-                    opacity = if (props.isOpen) 1 else 0
-                }
-
-                +"${props.duration} days"
-            }
-
-            styledDiv {
-                css {
-                    flexGrow = 10.0
-                    flexBasis = FlexBasis.zero
-                    alignSelf = Align.center
-                    color = Color.kodein.kaumon
-                }
-
-                flexRow(justifyContent = JustifyContent.center) {
-                    css {
-                        "span.chevron" {
-                            position = Position.relative
-                            textAlign = TextAlign.center
-                            height = 3.px
-                            width = 30.px
-                        }
-
-                        "span.chevron:before" {
-                            content = QuotedString("")
-                            position = Position.absolute
-                            top = 0.px
-                            left = 0.px
-                            height = 100.pct
-                            width = 51.pct
-                            backgroundColor = Color.kodein.klycine.withAlpha(0.75)
-                            transform { skew(0.deg, (30).deg); }
-                        }
-
-                        "span.chevron:after" {
-                            content = QuotedString("")
-                            position = Position.absolute
-                            top = 0.px
-                            right = 0.px
-                            height = 100.pct
-                            width = 50.pct
-                            backgroundColor = Color.kodein.klycine.withAlpha(0.75)
-                            transform { skew(0.deg, (-30).deg); }
-                        }
-
-                        "span.chevron.up:before" {
-                            transform { skew(0.deg, (-30).deg); }
-                        }
-
-                        "span.chevron.up:after" {
-                            transform { skew(0.deg, (30).deg); }
-                        }
-                    }
-
-                    span(if (props.isOpen) "chevron up" else "chevron") {}
-                }
-            }
-        }
-
-        styledDiv {
-            css {
-                overflow = Overflow.hidden
-
-                transition(::opacity, duration = .3.s, Timing.linear)
-
-                if (props.isOpen) {
-                    maxHeight = 50.rem
-                    opacity = 1
-                } else {
-                    maxHeight = 0.rem
-                    opacity = 0
-                }
-            }
-
-            styledP {
-                css {
-                    +kodein.body
-                    color = Color.kodein.kaumon
-                    padding(2.rem)
-                }
-
-                props.children()
-                +"Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                br {  }
-                +"- Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
-                br {  }
-                +"- when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-            }
+            width = 1.rem
+            height = 1.rem
+            paddingRight = .5.rem
         }
     }
-
+    +duration
 }
-
-data class Trai(val title: String, val description: String)
-val trainingList = listOf(
-    Trai("Kotlin for Android", ""),
-    Trai("Working with coroutines", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
-    Trai("Succeeding your next KMM project", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-)
